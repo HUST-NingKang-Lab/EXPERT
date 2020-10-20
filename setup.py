c2f3a46857b4c9ae16c5b7f9dafa13dd432c78ed
AdeBC <huichong.me@gmail.com>
2020-10-20 17:21:48 +0800

    Sync

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ DESCRIPTION = "your project description"
 if __name__ == "__main__":
     setup(
         name=NAME,
-        version="0.1-rc0",
+        version="0.1",
         author=AUTHOR,
         author_email=EMAIL,
         url=URL,
